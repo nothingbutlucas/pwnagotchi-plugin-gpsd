28aa8dd027fb1ad356bbaf9a48c3a256c7333d24
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-02-13 17:04:21 -0300

    feat(Defensive programming): Try, excepts, logs. Also support for waveshare 1.54 inches v2
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

--- gpsd.py
+++ gpsd.py
@@ -47,131 +47,156 @@ class gpsd_coord(plugins.Plugin):
         self.gpsd = None
 
     def on_loaded(self):
-        self.gpsd = GPSD(self.options["gpsdhost"], self.options["gpsdport"])
-        logging.info("[gpsd] plugin loaded")
+        try:
+            self.gpsd = GPSD(self.options["gpsdhost"], self.options["gpsdport"])
+            logging.info("[GPSD] plugin loaded")
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
 
     def on_ready(self, agent):
-        if self.options["gpsdhost"]:
-            logging.info(
-                f"enabling bettercap's gps module for {self.options['gpsdhost']}:{self.options['gpsdport']}"
-            )
-            try:
-                agent.run("gps off")
-            except Exception:
-                logging.info(f"bettercap gps was already off")
-                pass
+        try:
+            if self.options["gpsdhost"]:
+                logging.info(
+                    f"enabling bettercap's gps module for {self.options['gpsdhost']}:{self.options['gpsdport']}"
+                )
+                try:
+                    agent.run("gps off")
+                except Exception:
+                    logging.info(f"bettercap gps was already off")
+                    pass
 
-            agent.run("set gps.device 127.0.0.1:2947; set gps.baudrate 9600; gps on")
-            logging.info("bettercap set and on")
-            self.running = True
-        else:
-            logging.warning("no GPS detected")
+                agent.run(
+                    "set gps.device 127.0.0.1:2947; set gps.baudrate 9600; gps on"
+                )
+                logging.info("bettercap set and on")
+                self.running = True
+            else:
+                logging.warning("no GPS detected")
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
 
     def on_handshake(self, agent, filename, access_point, client_station):
-        coords = self.gpsd.update_gps()
-        if coords and all(
-            [
-                # avoid 0.000... measurements
-                coords["Latitude"],
-                coords["Longitude"],
-            ]
-        ):
-            gps_filename = filename.replace(".pcap", ".gps.json")
-            logging.info(f"[gpsd] saving GPS to {gps_filename} ({coords})")
-            with open(gps_filename, "w+t") as fp:
-                json.dump(coords, fp)
-        else:
-            logging.info("[gpsd] not saving GPS: no fix")
+        try:
+            coords = self.gpsd.update_gps()
+            if coords and all(
+                [
+                    # avoid 0.000... measurements
+                    coords["Latitude"],
+                    coords["Longitude"],
+                ]
+            ):
+                gps_filename = filename.replace(".pcap", ".gps.json")
+                logging.info(f"[GPSD] saving GPS to {gps_filename} ({coords})")
+                with open(gps_filename, "w+t") as fp:
+                    json.dump(coords, fp)
+            else:
+                logging.info("[GPSD] not saving GPS: no fix")
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
 
     def on_ui_setup(self, ui):
-        # add coordinates for other displays
-        if ui.is_waveshare_v2():
-            lat_pos = (127, 75)
-            lon_pos = (122, 84)
-            alt_pos = (127, 94)
-        elif ui.is_waveshare_v1():
-            lat_pos = (130, 70)
-            lon_pos = (125, 80)
-            alt_pos = (130, 90)
-        elif ui.is_inky():
-            lat_pos = (127, 60)
-            lon_pos = (127, 70)
-            alt_pos = (127, 80)
-        elif ui.is_waveshare144lcd():
-            # guessed values, add tested ones if you can
-            lat_pos = (67, 73)
-            lon_pos = (62, 83)
-            alt_pos = (67, 93)
-        elif ui.is_dfrobot_v2:
-            lat_pos = (127, 75)
-            lon_pos = (122, 84)
-            alt_pos = (127, 94)
-        elif ui.is_waveshare27inch():
-            lat_pos = (6, 120)
-            lon_pos = (1, 135)
-            alt_pos = (6, 150)
-        else:
-            # guessed values, add tested ones if you can
-            lat_pos = (127, 51)
-            lon_pos = (127, 56)
-            alt_pos = (102, 71)
+        try:
+            # add coordinates for other displays
+            if ui.is_waveshare_v2():
+                lat_pos = (127, 75)
+                lon_pos = (122, 84)
+                alt_pos = (127, 94)
+            elif ui.is_waveshare_v1():
+                lat_pos = (130, 70)
+                lon_pos = (125, 80)
+                alt_pos = (130, 90)
+            elif ui.is_inky():
+                lat_pos = (127, 60)
+                lon_pos = (127, 70)
+                alt_pos = (127, 80)
+            elif ui.is_waveshare144lcd():
+                # guessed values, add tested ones if you can
+                lat_pos = (67, 73)
+                lon_pos = (62, 83)
+                alt_pos = (67, 93)
+            elif ui.is_dfrobot_v2:
+                lat_pos = (127, 75)
+                lon_pos = (122, 84)
+                alt_pos = (127, 94)
+            elif ui.is_waveshare27inch():
+                lat_pos = (6, 120)
+                lon_pos = (1, 135)
+                alt_pos = (6, 150)
+            elif ui.is_waveshare1in54V2():
+                lat_pos = (145, 110)
+                lon_pos = (145, 120)
+                alt_pos = (145, 130)
+            else:
+                # guessed values, add tested ones if you can
+                lat_pos = (127, 51)
+                lon_pos = (127, 56)
+                alt_pos = (102, 71)
 
-        label_spacing = 0
+            label_spacing = 0
 
-        ui.add_element(
-            "latitude",
-            LabeledValue(
-                color=BLACK,
-                label="lat:",
-                value="-",
-                position=lat_pos,
-                label_font=fonts.Small,
-                text_font=fonts.Small,
-                label_spacing=label_spacing,
-            ),
-        )
-        ui.add_element(
-            "longitude",
-            LabeledValue(
-                color=BLACK,
-                label="long:",
-                value="-",
-                position=lon_pos,
-                label_font=fonts.Small,
-                text_font=fonts.Small,
-                label_spacing=label_spacing,
-            ),
-        )
-        ui.add_element(
-            "altitude",
-            LabeledValue(
-                color=BLACK,
-                label="alt:",
-                value="-",
-                position=alt_pos,
-                label_font=fonts.Small,
-                text_font=fonts.Small,
-                label_spacing=label_spacing,
-            ),
-        )
+            ui.add_element(
+                "latitude",
+                LabeledValue(
+                    color=BLACK,
+                    label="lat:",
+                    value="-",
+                    position=lat_pos,
+                    label_font=fonts.Small,
+                    text_font=fonts.Small,
+                    label_spacing=label_spacing,
+                ),
+            )
+            ui.add_element(
+                "longitude",
+                LabeledValue(
+                    color=BLACK,
+                    label="long:",
+                    value="-",
+                    position=lon_pos,
+                    label_font=fonts.Small,
+                    text_font=fonts.Small,
+                    label_spacing=label_spacing,
+                ),
+            )
+            ui.add_element(
+                "altitude",
+                LabeledValue(
+                    color=BLACK,
+                    label="alt:",
+                    value="-",
+                    position=alt_pos,
+                    label_font=fonts.Small,
+                    text_font=fonts.Small,
+                    label_spacing=label_spacing,
+                ),
+            )
+
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
 
     def on_unload(self, ui):
-        with ui._lock:
-            ui.remove_element("latitude")
-            ui.remove_element("longitude")
-            ui.remove_element("altitude")
+        try:
+            with ui._lock:
+                ui.remove_element("latitude")
+                ui.remove_element("longitude")
+                ui.remove_element("altitude")
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
 
     def on_ui_update(self, ui):
-        coords = self.gpsd.update_gps()
-        if coords and all(
-            [
-                # avoid 0.000... measurements
-                coords["Latitude"],
-                coords["Longitude"],
-            ]
-        ):
-            # last char is sometimes not completely drawn
-            # using an ending-whitespace as workaround on each line
-            ui.set("latitude", f"{coords['Latitude']:.4f} ")
-            ui.set("longitude", f" {coords['Longitude']:.4f} ")
-            ui.set("altitude", f" {coords['Altitude']:.1f}m ")
+        try:
+            coords = self.gpsd.update_gps()
+            if coords and all(
+                [
+                    # avoid 0.000... measurements
+                    coords["Latitude"],
+                    coords["Longitude"],
+                ]
+            ):
+                # last char is sometimes not completely drawn
+                # using an ending-whitespace as workaround on each line
+                ui.set("latitude", f"{coords['Latitude']:.4f} ")
+                ui.set("longitude", f" {coords['Longitude']:.4f} ")
+                ui.set("altitude", f" {coords['Altitude']:.1f}m ")
+        except Exception as e:
+            logging.error(f"[GPSD] Error: {e}")
